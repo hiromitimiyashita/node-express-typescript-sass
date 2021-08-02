dc540c7f44d7f9d2850b159f6588f7ea00cb8643
Alex Alves <hiromiti1982@gmail.com>
2021-08-03 08:10:20 +0900

    Controllers e Models

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response)=>{
-    res.render('home', {nome: 'Alex'});
+    res.render('pages/home', {nome: 'Alex'});
 });
 
 router.get('/contato', ()=>{});
